bc74f9154dc82647c8b82c3dde883d637049f379
byrd <jordan@baltimorehealthanalytics.com>
2019-10-29 08:17:42 -0400

    completed styling w media query

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,17 @@
 import { Link } from "gatsby"
-// import PropTypes from "prop-types"
 import React from "react"
-import logo from '../images/logo-designcode.svg'
+// import logo from '../images/logo-designcode.svg'
+// the above will be imported in the link tag
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
  <div className="Header">
    <div className="HeaderGroup">
-    <Link to="/"><img src={logo} width="" /></Link>
+    <Link to="/"><img src={require('../images/logo-designcode.svg')} width="" /></Link>
     <Link to="/courses">Courses</Link>
     <Link to="/downloads">Download</Link>
     <Link to="/workshops">Workshops</Link>
-    <Link to="/buy"><button>Button</button></Link>
+    <Link to="/buy"><button>Buy</button></Link>
    </div>
  </div> 
 )
